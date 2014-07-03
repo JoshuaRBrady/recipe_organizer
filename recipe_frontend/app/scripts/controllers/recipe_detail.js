5e728b5af21862b516766b7d66b37d6ad260b4f6
Joshua Brady <brady.joshuar@gmail.com>
2014-07-03 11:53:07 -0600

    Updating styling and models.

--- recipe_frontend/app/scripts/controllers/recipe_detail.js
+++ recipe_frontend/app/scripts/controllers/recipe_detail.js
@@ -15,3 +15,25 @@ angular.module('recipeOrganizerApp')
             $scope.recipe = data;
         });
     });
+
+angular.module('recipeOrganizerApp')
+    .controller('RecipeDetailCtrl', function ($scope, $routeParams, Restangular, $location) {
+        $scope.recipeId = $routeParams.recipeId;
+
+        Restangular.one('recipes', $scope.recipeId).customGET().then(function (data) {
+            $scope.recipe = data;
+        console.log(data);
+        });
+
+        $scope.deleteRecipe = function() {
+            var response = confirm("Are you sure you want to delete this recipe?");
+            if (response == true) {
+                Restangular.one('recipes', $scope.recipeId).customDELETE().then(function () {
+                    $location.path('/recipes');
+                }, function() {
+                    $scope.status = "The recipe couldn't be deleted";
+                });
+            }
+        };
+
+    });
